dd8b26e4a1bd9df8f3972a27d5feadc9f183f01f
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-11 12:35:07 -0500

    adds jwt decoding

--- stocky-server/app/services/jwt_service.rb
+++ stocky-server/app/services/jwt_service.rb
@@ -2,4 +2,13 @@ class JwtService
   def self.encode(payload)
     JWT.encode(payload, Rails.application.credentials[:secret_key_base], 'HS256')
   end
+
+  def self.decode(token)
+    if(token)
+      body, = JWT.decode(token, Rails.application.credentials[:secret_key_base], true, algorithm: 'HS256')
+      HashWithIndifferentAccess.new(body)
+    end
+  rescue JWT::ExpiredSignature
+    nil
+  end
 end
