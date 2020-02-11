39bf09be10d99c5e7e9282a2d705ea48ca123c75
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-11 12:31:01 -0500

    adds jwt service basic encode/decode tests

---
+++ stocky-server/spec/services/jwt_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe JwtService, :type => :service do
+  
+  it "encodes a hash into a string that does not include the information" do
+    expect(JwtService.encode({ user_id: 1 })).to_not eq({ user_id: 1 })
+    expect(JwtService.encode({ user_id: 1 })).to be_instance_of(String)
+    expect(JwtService.encode({ user_id: 1 })).to_not be_include("user_id: 1")
+  end
+
+  context "when a hash is encoded" do
+    before do
+      @token = JwtService.encode({ user_id: 1 })
+    end
+
+    it "decodes the encoded string into its original form" do
+      expect(JwtService.decode(@token)).to eq({ user_id: 1 })
+    end
+  end
+
+end
