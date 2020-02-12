f176e424a2da0a7f2bdad7a237a2aacc3a597cdc
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-11 20:06:54 -0500

    adds before action to fetch current user if token in header

--- stocky-server/app/controllers/application_controller.rb
+++ stocky-server/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :fetch_current_user
+
+  def fetch_current_user
+    decoded_hash = JwtService.decode(request.headers[:Token])
+    @current_user_id = decoded_hash[:user_id] if decoded_hash
+  end
+
 end
