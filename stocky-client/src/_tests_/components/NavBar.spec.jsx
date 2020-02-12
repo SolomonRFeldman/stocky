a74adffb0341b950a099fc24c9a19851b3545feb
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-11 22:12:20 -0500

    async renders app while awaiting promise return

--- stocky-client/src/_tests_/components/NavBar.spec.jsx
+++ stocky-client/src/_tests_/components/NavBar.spec.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from '../../App'
+import { act } from 'react-dom/test-utils'
 
-it('renders the web app name', () => {
-  const navBar = render(<App />).getByLabelText('Navbar')
+let navBar
+
+it('renders the web app name', async() => {
+  await act( async() => navBar = render(<App />).getByLabelText('Navbar'))
   expect(navBar).toHaveTextContent('STOCKY')  
 })
