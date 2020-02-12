2535eaffc6def93bd95829faf083e71cc79beba2
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-11 21:35:57 -0500

    adds navbar w/ brand

---
+++ stocky-client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ stocky-client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+export default function NavBar(props) {
+  return(
+    <Navbar bg='dark' variant='dark'>
+      <Navbar.Brand >Stocky</Navbar.Brand>
+    </Navbar>
+  )
+}
