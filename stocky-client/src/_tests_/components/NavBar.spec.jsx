7d70e156ddae5251e30a7fffa83e3037040a685c
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-11 23:48:52 -0500

    adds test for signup button popping up modal

--- stocky-client/src/_tests_/components/NavBar.spec.jsx
+++ stocky-client/src/_tests_/components/NavBar.spec.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent, within } from '@testing-library/react'
 import App from '../../App'
 import { act } from 'react-dom/test-utils'
 
@@ -8,4 +8,11 @@ let navBar
 it('renders the web app name', async() => {
   await act( async() => navBar = render(<App />).getByLabelText('Navbar'))
   expect(navBar).toHaveTextContent('STOCKY')  
+})
+
+it('has a sign up button that opens a sign up modal/form', async() => {
+  await act( async() => navBar = render(<App />).getByLabelText('Navbar'))
+  const signUpButton = within(navBar).getByLabelText('Sign Up')
+  await act( async() => fireEvent.click(signUpButton))
+  expect(within(document).getByLabelText('Sign Up Modal')).toBeInTheDocument()
 })
