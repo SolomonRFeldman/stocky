71e562ad1f93eab1f7fccc13e35f4b49cd15eb4d
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-13 00:03:13 -0500

    wraps app in provider w/ manageCurrentUser store

--- stocky-client/src/App.jsx
+++ stocky-client/src/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import 'bootswatch/dist/journal/bootstrap.min.css';
+
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import manageCurrentUser from './reducers/manageCurrentUser';
+import thunk from 'redux-thunk'
+
 import NavBar from './components/NavBar/NavBar';
+
+const store = createStore(
+  manageCurrentUser,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <NavBar />
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header>
+          <NavBar />
+        </header>
+      </div>
+    </Provider>
   );
 }
 
