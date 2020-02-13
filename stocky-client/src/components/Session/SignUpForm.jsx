5fc848662768e40e66bfae7dfeaf43e65ff3e2e0
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-12 23:03:28 -0500

    adds form data collection and posting to api

--- stocky-client/src/components/Session/SignUpForm.jsx
+++ stocky-client/src/components/Session/SignUpForm.jsx
@@ -1,34 +1,43 @@
 import React, { useState } from 'react'
 import { Modal, Form, Button } from 'react-bootstrap'
+import { postRequest } from '../../apiRequests'
 
 export default function SignUpForm(props) {
+  const [formData, setFormData] = useState({ name: '', email: '', password: '', password_confirmation: ''})
+  const handleChange = event => setFormData({ ...formData, [event.target.id]: event.target.value })
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    return postRequest('/signup', { user: formData })
+  }
 
   return(
     <Modal aria-label='Sign Up Modal' show={props.show} onHide={props.handleClose} centered>
       <Modal.Header closeButton>
         <Modal.Title>Sign up</Modal.Title>
       </Modal.Header>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Modal.Body>
-          <Form.Group>
+          <Form.Group controlId='name'>
             <Form.Label>Name</Form.Label>
-            <Form.Control id='name' />
+            <Form.Control placeholder='Name' onChange={handleChange} />
           </Form.Group>
-          <Form.Group>
+          <Form.Group controlId='email'>
             <Form.Label>Email</Form.Label>
-            <Form.Control id='email' type='email' />
+            <Form.Control placeholder='Email' type='email' onChange={handleChange} />
           </Form.Group>
-          <Form.Group>
+          <Form.Group controlId='password'>
             <Form.Label>Password</Form.Label>
-            <Form.Control id='password' type='password' />
+            <Form.Control placeholder='Password' type='password' onChange={handleChange} />
           </Form.Group>
-          <Form.Group>
+          <Form.Group controlId='password_confirmation'>
             <Form.Label>Password Confirmation</Form.Label>
-            <Form.Control id='password_confirmation' type='password' />
+            <Form.Control placeholder='Password Confirmation' type='password' onChange={handleChange} />
           </Form.Group>
         </Modal.Body>
         <Modal.Footer>
-          <Button type='submit'>Sign Up</Button>
+          <Button aria-label='Submit Sign Up' type='submit'>Sign Up</Button>
         </Modal.Footer>
       </Form>
     </Modal>
