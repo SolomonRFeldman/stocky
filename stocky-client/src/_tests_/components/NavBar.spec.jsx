d4cbc611584d19d9b0dc2baec689bf1108f62873
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-12 23:35:38 -0500

    adds sign up response handling test

--- stocky-client/src/_tests_/components/NavBar.spec.jsx
+++ stocky-client/src/_tests_/components/NavBar.spec.jsx
@@ -5,7 +5,9 @@ import { act } from 'react-dom/test-utils'
 import fetchMock from 'fetch-mock'
 
 let navBar
+
 const mockLoginResponse = { user: { id: 1, name: "Test", token: "totesarealtoken"}}
+fetchMock.post('/signup', mockLoginResponse)
 
 it('renders the web app name', async() => {
   await act( async() => navBar = render(<App />).getByLabelText('Navbar'))
@@ -24,8 +26,6 @@ it('sends the correct data to the server when the signup form is filled and subm
   const signUpButton = within(navBar).getByLabelText('Sign Up')
   await act(async() => fireEvent.click(signUpButton))
   const signUpForm = within(document).getByLabelText('Sign Up Modal')
-
-  const signInMock = fetchMock.postOnce('/signup', mockLoginResponse)
   
   await act(async() => fireEvent.change(within(signUpForm).getByLabelText('Name'), { target: { value: 'Test' } }))
   await act(async() => fireEvent.change(within(signUpForm).getByLabelText('Email'), { target: { value: 'Test@123.com' } }))
@@ -39,4 +39,21 @@ it('sends the correct data to the server when the signup form is filled and subm
   expect(params.email).toBe(expectedCall.email)
   expect(params.password).toBe(expectedCall.password)
   expect(params.password_confirmation).toBe(expectedCall.password_confirmation)
+})
+
+it('logs the user in, displays their name in the banner with a logout button, and closes the modal when they sign up', async() => {
+  await act(async() => navBar = render(<App />).getByLabelText('Navbar'))
+  const signUpButton = within(navBar).getByLabelText('Sign Up')
+  await act(async() => fireEvent.click(signUpButton))
+  const signUpForm = within(document).getByLabelText('Sign Up Modal')
+  
+  await act(async() => fireEvent.change(within(signUpForm).getByLabelText('Name'), { target: { value: 'Test' } }))
+  await act(async() => fireEvent.change(within(signUpForm).getByLabelText('Email'), { target: { value: 'Test@123.com' } }))
+  await act(async() => fireEvent.change(within(signUpForm).getByLabelText('Password'), { target: { value: '123' } }))
+  await act(async() => fireEvent.change(within(signUpForm).getByLabelText('Password Confirmation'), { target: { value: '123' } }))
+  await act(async() => fireEvent.click(within(signUpForm).getByLabelText('Submit Sign Up')))
+
+  expect(navBar).toHaveTextContent('Welcome, Test')
+  expect(within(navBar).getByLabelText('Log Out')).toBeInTheDocument()
+  expect(signUpForm).not.toBeInTheDocument()
 })
