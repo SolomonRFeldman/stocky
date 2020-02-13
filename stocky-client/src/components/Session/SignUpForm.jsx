c3f81b120cdd3e3096056bf7c790a0aee72f6407
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-12 22:30:42 -0500

    adds signup form fields

--- stocky-client/src/components/Session/SignUpForm.jsx
+++ stocky-client/src/components/Session/SignUpForm.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import { Modal } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
 
 export default function SignUpForm(props) {
+
   return(
     <Modal aria-label='Sign Up Modal' show={props.show} onHide={props.handleClose} centered>
       <Modal.Header closeButton>
         <Modal.Title>Sign up</Modal.Title>
       </Modal.Header>
+      <Form>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control id='name' />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control id='email' type='email' />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control id='password' type='password' />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control id='password_confirmation' type='password' />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button type='submit'>Sign Up</Button>
+        </Modal.Footer>
+      </Form>
     </Modal>
   )
 }
