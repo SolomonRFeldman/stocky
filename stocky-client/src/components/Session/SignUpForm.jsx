22b33e09ce600c10f6004f3b96e6f703bcdabacd
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-12 23:36:05 -0500

    adds adding token to local storage on submit

--- stocky-client/src/components/Session/SignUpForm.jsx
+++ stocky-client/src/components/Session/SignUpForm.jsx
@@ -9,7 +9,10 @@ export default function SignUpForm(props) {
   const handleSubmit = event => {
     event.preventDefault()
 
-    return postRequest('/signup', { user: formData })
+    return postRequest('/signup', { user: formData }).then(user => {
+      localStorage.token = user.token
+      props.handleClose()
+    })
   }
 
   return(
