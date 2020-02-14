60f36523e3d858371a51fa016c79a518e64d364d
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-13 19:49:30 -0500

    moves store to index comp

--- stocky-client/src/App.jsx
+++ stocky-client/src/App.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import 'bootswatch/dist/journal/bootstrap.min.css';
-
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux';
-import manageCurrentUser from './reducers/manageCurrentUser';
-import thunk from 'redux-thunk'
-
 import NavBar from './components/NavBar/NavBar';
 
-const store = createStore(
-  manageCurrentUser,
-  compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-  )
-)
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <header>
-          <NavBar />
-        </header>
-      </div>
-    </Provider>
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+    </div>
   );
 }
 
