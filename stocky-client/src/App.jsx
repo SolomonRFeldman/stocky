05ebc7e353bbf82a5eb5c150c1acfce026c49645
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-14 09:24:14 -0500

    adds auto login on return

--- stocky-client/src/App.jsx
+++ stocky-client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bootswatch/dist/journal/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
+import { useDispatch } from 'react-redux';
+import { postRequest } from './apiRequests';
 
+export default function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const addCurrentUser = payload => dispatch({ type: 'ADD_CURRENT_USER', payload})
+    if(localStorage.token) {
+      postRequest('/login').then(user => {
+        localStorage.token = user.token
+        addCurrentUser({ id: user.id, name: user.name })
+      })
+    }
+  }, [])
 
-function App() {
   return (
     <div className="App">
       <header>
@@ -12,5 +24,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
