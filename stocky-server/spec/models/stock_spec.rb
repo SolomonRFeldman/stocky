67ee02e89c917aa8ba997495d8f4564f5b21bbcd
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-15 13:09:25 -0500

    adds stock to user relation tests

--- stocky-server/spec/models/stock_spec.rb
+++ stocky-server/spec/models/stock_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Stock, :type => :model do
   let(:valid_stock) { create(:valid_stock) }
+  let(:valid_user) { create(:valid_user) }
+  let(:jelly_user) { create(:jelly_user) }
 
   it('is valid with a symbol') do
     expect(valid_stock).to be_valid
@@ -13,6 +15,23 @@ RSpec.describe Stock, :type => :model do
 
   it "is invalid with a nil symbol" do
     expect(build(:valid_stock, symbol: nil)).to_not be_valid
+  end
+
+  context 'when a user is joined to stocks' do
+    before do
+      @user_stock_1 = UserStock.create(user_id: valid_user.id, stock_id: valid_stock.id)
+      @user_stock_2 = UserStock.create(user_id: jelly_user.id, stock_id: valid_stock.id)
+    end
+
+    it 'has many user_stocks' do
+      expect(valid_stock.user_stocks).to be_include(@user_stock_1)
+      expect(valid_stock.user_stocks).to be_include(@user_stock_2)
+    end
+
+    it 'has many users' do
+      expect(valid_stock.users).to be_include(valid_user)
+      expect(valid_stock.users).to be_include(jelly_user)
+    end
   end
 
 end
