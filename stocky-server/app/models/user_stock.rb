96e1b43d69f3b7b3bb5666968c1682216c619716
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-15 13:47:47 -0500

    find_or_create_by_symbol creates the stock if it doesn't exist

--- stocky-server/app/models/user_stock.rb
+++ stocky-server/app/models/user_stock.rb
@@ -10,7 +10,7 @@ class UserStock < ApplicationRecord
           .joins('JOIN stocks ON user_stocks.stock_id = stocks.id')
           .where("user_id = ? AND stocks.symbol = ?", user_id, stock_symbol)
           .first
-      user_stock ? user_stock : UserStock.create(user_id: user_id, stock_id: Stock.find_by(symbol: stock_symbol).id)
+      user_stock ? user_stock : UserStock.create(user_id: user_id, stock_id: Stock.find_or_create_by(symbol: stock_symbol).id)
     end
 
   end
