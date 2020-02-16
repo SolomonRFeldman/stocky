dc7a82376a7986ec1541f6200a10e2c7f2121061
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-16 10:59:46 -0500

    adds instance method version of .with_symbol

--- stocky-server/app/models/user_stock_history.rb
+++ stocky-server/app/models/user_stock_history.rb
@@ -2,6 +2,13 @@ class UserStockHistory < ApplicationRecord
   belongs_to :user
   belongs_to :stock
 
+  def with_symbol
+    self
+      .attributes
+      .slice("id", "shares", "price")
+      .merge("symbol" => self.stock.symbol)
+  end
+
   class << self
 
     def with_symbol(user_stock_histories)
