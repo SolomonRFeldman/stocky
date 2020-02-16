a257721b6d5d6af733b65702b6be9f8024e7542e
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-15 21:31:45 -0500

    adds association tests

--- stocky-server/spec/models/user_stock_history_spec.rb
+++ stocky-server/spec/models/user_stock_history_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe UserStockHistory, :type => :model do
     UserStockHistory.create(user_id: valid_user.id, stock_id: valid_stock.id, price: -200, shares: -2)
   end
 
+  context 'when it is created' do
+    before do
+      @user_stock_history = UserStockHistory.create(user_id: valid_user.id, stock_id: valid_stock.id, price: 200, shares: 2)
+    end
+
+    it 'has a user' do
+      expect(@user_stock_history.user).to eq(valid_user)
+    end
+
+    it 'has a stock' do
+      expect(@user_stock_history.stock).to eq(valid_stock)
+    end
+  end
+
 end
