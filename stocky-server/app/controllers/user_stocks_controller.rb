c00d06817352597f37791d4957dbf1f9f4b39199
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-16 14:59:07 -0500

    returns new user balance on stock transaction

--- stocky-server/app/controllers/user_stocks_controller.rb
+++ stocky-server/app/controllers/user_stocks_controller.rb
@@ -5,7 +5,11 @@ class UserStocksController < ApplicationController
       user_stock = UserStock.find_or_new_by_symbol(user_id: params[:user_id], stock_symbol: params[:user_stock][:symbol])
       user_stock.shares += params[:user_stock][:shares].to_i
       user_stock.save
-      return render json: { **user_stock.with_prices, user_stock_history: user_stock.last_history.with_symbol }, status: 200
+      return render json: { 
+        new_balance: user_stock.user.balance, 
+        **user_stock.with_prices, 
+        user_stock_history: user_stock.last_history.with_symbol 
+      }, status: 200
     end
     render status: 400
   end
