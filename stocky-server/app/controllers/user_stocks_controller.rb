b23e305750bfd69d59f76e287a0d778eef760eaa
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-16 10:53:42 -0500

    adds user_stock post req creation

---
+++ stocky-server/app/controllers/user_stocks_controller.rb
@@ -0,0 +1,13 @@
+class UserStocksController < ApplicationController
+
+  def create
+    if @current_user_id == params[:user_id].to_i
+      user_stock = UserStock.find_or_new_by_symbol(user_id: params[:user_id], stock_symbol: params[:user_stock][:symbol])
+      user_stock.shares += params[:user_stock][:shares].to_i
+      user_stock.save
+      return render json: user_stock.with_prices, status: 200
+    end
+    render status: 400
+  end
+
+end
--- stocky-server/config/routes.rb
+++ stocky-server/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   post '/login' => 'sessions#create'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :user_stocks, only: [:create]
+  end
 
 end
