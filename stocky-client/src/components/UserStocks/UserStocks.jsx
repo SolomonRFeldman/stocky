5d8f99e70a562f3c5d229c496f61b10dd70d7b1e
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-16 14:36:34 -0500

    fetches user show data and stores in user state

--- stocky-client/src/components/UserStocks/UserStocks.jsx
+++ stocky-client/src/components/UserStocks/UserStocks.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './UserStocks.css'
 import { CardGroup, Card } from 'react-bootstrap'
 import UserStockForm from './UserStockForm'
+import { getRequest } from '../../apiRequests'
+import { useSelector } from 'react-redux'
 
 export default function UserStocks(props) {
+  const currentUser = useSelector(state => state.currentUser)
+  const [user, setUser] = useState({})
+  
+  useEffect(() =>{
+    getRequest(`/users/${currentUser.id}`).then(fetchedUser => setUser(fetchedUser))
+  }, [])
+
   return(
     <CardGroup className='mx-auto mt-4 user-stocks'>
       <Card>
       </Card>
       <Card>
         <Card.Body>
-          <UserStockForm />
+          <UserStockForm user={user} />
         </Card.Body>
       </Card>
     </CardGroup>
