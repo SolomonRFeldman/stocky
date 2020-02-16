0d4ce7c57e32177e6601f77633d6af777ed9988a
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-16 08:30:13 -0500

    adds user stock history show parser

--- stocky-server/app/models/user_stock_history.rb
+++ stocky-server/app/models/user_stock_history.rb
@@ -2,4 +2,13 @@ class UserStockHistory < ApplicationRecord
   belongs_to :user
   belongs_to :stock
 
+  class << self
+
+    def with_symbol(user_stock_histories)
+      user_stock_histories
+        .joins('JOIN stocks ON user_stock_histories.stock_id = stocks.id')
+        .select(:id, :symbol, :shares, :price)
+    end
+
+  end
 end
