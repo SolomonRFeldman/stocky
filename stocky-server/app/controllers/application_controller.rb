2a7353abe34ea828f31368db4814a72071467634
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-15 22:46:24 -0500

    sets fetch_current_user as a private function

--- stocky-server/app/controllers/application_controller.rb
+++ stocky-server/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
   before_action :fetch_current_user
 
+  private
+
   def fetch_current_user
     decoded_hash = JwtService.decode(request.headers[:Token])
     @current_user_id = decoded_hash[:user_id] if decoded_hash
