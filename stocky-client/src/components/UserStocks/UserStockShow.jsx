6f36108196a06c372d834c7cc9a5516890100715
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-17 14:42:49 -0500

    adds colors to value text based on comparision with open

--- stocky-client/src/components/UserStocks/UserStockShow.jsx
+++ stocky-client/src/components/UserStocks/UserStockShow.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import './UserStockShow.css'
 import { ListGroupItem } from 'react-bootstrap'
 
 export default function UserStockShow({ userStock }) {
+
+  const stockValueHighlight = () => {
+    if(userStock.latestPrice > userStock.open) {
+      return '-green'
+    } else if(userStock.latestPrice < userStock.open) {
+      return '-red'
+    } else {
+      return ''
+    }
+  }
+
   return(
-    <ListGroupItem>
+    <ListGroupItem className='user-stock-show'>
       {userStock.symbol} - {userStock.shares} Shares
-      <span className='float-right'>${(userStock.latestPrice * userStock.shares).toFixed(2)}</span>
+      <span className={`float-right value-span${stockValueHighlight()}`}>
+        ${(userStock.latestPrice * userStock.shares).toFixed(2)}
+      </span>
     </ListGroupItem>
   )
 }
