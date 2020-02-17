09836f27cdfe5201ea0699e069419a0d75d69446
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-17 10:58:22 -0500

    adds basic route to portfolio

--- stocky-client/src/components/UserStocks/UserStocks.jsx
+++ stocky-client/src/components/UserStocks/UserStocks.jsx
@@ -4,18 +4,25 @@ import { CardGroup, Card } from 'react-bootstrap'
 import UserStockForm from './UserStockForm'
 import { getRequest } from '../../apiRequests'
 import { useSelector } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import UserStocksShow from './UserStocksShow'
 
 export default function UserStocks(props) {
   const currentUser = useSelector(state => state.currentUser)
-  const [user, setUser] = useState({balance: 0})
+  const [user, setUser] = useState({balance: 0, user_stocks: []})
   
   useEffect(() =>{
     getRequest(`/users/${currentUser.id}`).then(fetchedUser => setUser(fetchedUser))
   }, [])
 
+  const userStocksShow = () => <UserStocksShow userStocks={user.user_stocks} />
+
   return(
     <CardGroup className='mx-auto mt-4 user-stocks'>
       <Card>
+        <Router>
+          <Route exact path='/' component={userStocksShow} />
+        </Router>
       </Card>
       <Card>
         <Card.Body>
---
+++ stocky-client/src/components/UserStocks/UserStocksShow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserStocksShow({userStocks}) {
+  return(
+    <div>
+      <div>Boo</div>
+    </div>
+  )
+}
