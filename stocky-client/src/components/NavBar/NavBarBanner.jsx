17b1b7ebae08645d702d02324d3bc3f712f0194e
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-17 12:04:56 -0500

    adds portfolio/transaction links

--- stocky-client/src/components/NavBar/NavBarBanner.jsx
+++ stocky-client/src/components/NavBar/NavBarBanner.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './NavBarBanner.css'
 import { Nav } from 'react-bootstrap'
 import LogOutButton from '../Session/LogOutButton'
+import { Link } from 'react-router-dom'
 
 export default function NavBarBanner({ className = '', currentUser }) {
   return(
     <Nav className={`nav-bar-banner ${className}`}>
+      <Nav.Link as={Link} to='/'>Portfolio</Nav.Link>
+      <Nav.Link as={Link} to='/transactions'>Transactions</Nav.Link>
       <Nav.Link className='active'>{`${currentUser.name}`}</Nav.Link>
       <LogOutButton className='log-out-button' />
     </Nav>
