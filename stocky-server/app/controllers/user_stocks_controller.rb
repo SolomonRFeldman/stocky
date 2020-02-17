1ab6e181160d705cd3f5f61ec08282967d9bf816
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-17 13:41:34 -0500

    if user_stock fails to save, return errors

--- stocky-server/app/controllers/user_stocks_controller.rb
+++ stocky-server/app/controllers/user_stocks_controller.rb
@@ -4,12 +4,15 @@ class UserStocksController < ApplicationController
     if @current_user_id == params[:user_id].to_i
       user_stock = UserStock.find_or_new_by_symbol(user_id: params[:user_id], stock_symbol: params[:user_stock][:symbol])
       user_stock.shares += params[:user_stock][:shares].to_i
-      user_stock.save
-      return render json: { 
-        new_balance: user_stock.user.balance, 
-        **user_stock.with_prices, 
-        user_stock_history: user_stock.last_history.with_symbol 
-      }, status: 200
+      if user_stock.save
+        return render json: { 
+          new_balance: user_stock.user.balance, 
+          **user_stock.with_prices, 
+          user_stock_history: user_stock.last_history.with_symbol 
+        }, status: 200
+      else
+        return render json: { errors: user_stock.errors }, status: 400
+      end
     end
     render status: 400
   end
