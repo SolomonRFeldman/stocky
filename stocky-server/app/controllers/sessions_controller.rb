72f72060a10b1779725813e210f3447296c82acb
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-17 12:37:04 -0500

    adds authentication error message

--- stocky-server/app/controllers/sessions_controller.rb
+++ stocky-server/app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     unless @current_user_id && user = User.find_by(id: @current_user_id)
       unless params[:user] && user = User.authenticate(email: params[:user][:email], password: params[:user][:password])
-        return render status: 400
+        return render json: { authentication_error: true }, status: 400
       end
     end
     render json: user.login_hash
